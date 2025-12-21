99fcd82796f80d646aabc4ac269dde91f0f667a3
Kushal <22raokushalyadav99@gmail.com>
2025-12-21 16:56:59 +0530

    Fix case-sensitive import: counter.js -> Counter.js

--- Capstone/backend/models/User.js
+++ Capstone/backend/models/User.js
@@ -1,7 +1,7 @@
 // models/User.js
 const mongoose = require("mongoose");
 const bcrypt   = require("bcryptjs");
-const Counter  = require("./counter.js");
+const Counter  = require("./Counter.js");
 
 const UserSchema = new mongoose.Schema({
   username: { type:String, required:true, unique:true, trim:true },
